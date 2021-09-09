631fb8249c5611a74e7f8e2a9ae127382e778ea0
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-09-09 09:47:22 -0400

    assignment upload

--- functions.js
+++ functions.js
@@ -1,13 +1,72 @@
+//////////////////////////////////////////////
 const identity = function (value) {
   return value;
 };
-const first = function (array, n) {};
 
-const last = function (array, n) {};
+//////////////////////////////////////////////
+const first = function (array, n) {
 
-const each = function (collection, callback) {};
-const indexOf = function (array, target) {};
-const map = function (collection, iterator) {};
+  if (n === undefined) {
+
+    return array[0];
+  
+  }
+
+  return array.slice(0, n)
+
+};
+
+//////////////////////////////////////////////
+const last = function (array, n) {
+
+  if (n === undefined) {
+    return array.pop(); 
+  }
+
+  if (n > array.length) {
+    return array;
+  }
+
+  if (n === 0) {
+    return [];
+  }
+
+  return array.slice(n - 1, array.length)
+
+};
+
+////////////////////////////////////////////////
+const each = function (collection, callback) {
+
+  if (collection instanceof Array) {
+
+    for (i = 0; i < collection.length; i++)
+    
+      callback(collection[i], i, collection);
+  
+  } else {
+
+    for (const animal in collection) {
+
+      callback(collection[animal], animal, collection);
+  
+    }
+  }
+};
+
+////////////////////////////////////////////////
+const indexOf = function (array, target) {
+
+  return array.findIndex((el) => el === target);
+
+};
+
+//////////////////////////////////////////////////
+const map = function (collection, iterator) {
+
+  return collection.map((el) => iterator(el));
+
+};
 
 module.exports = {
   identity,
